379aacbc974d41f03366f3bfd39e254dead5e218
gediminasnn <neniusgediminas@gmail.com>
2021-05-25 14:17:32 +0300

    implemented logger into project

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import log4js from "log4js";
 
 export default class Bootstrap {
   public app: express.Application;
   private port = process.env.API_PORT || 1338;
+  private logger: log4js.Logger = log4js.getLogger();
 
   constructor() {
     this.app = express();
+
+    this.logger.level = "debug";
+    this.logger.debug("This is debug line");
+    this.logger.info("This is info line");
+    this.logger.fatal("This is fatal line");
+    this.logger.error("This is error line");
 
     this.config();
     this.mount();
@@ -17,13 +25,14 @@ export default class Bootstrap {
   }
 
   private mount(): void {
-    console.log(this.port);
     this.app.get("/", (_, res: express.Response) => res.send("Hello World!"));
   }
 
   listen(): void {
-    this.app.listen(this.port, () =>
-      console.log(`Example app listening at http://localhost:${this.port}`)
-    );
+    this.app.listen(this.port, () => {
+      return console.log(
+        `Example app listening at http://localhost:${this.port}`
+      );
+    });
   }
 }
