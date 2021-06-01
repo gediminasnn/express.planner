d50894400e0e186e126beb966a2cbba74fe5690f
gediminasnn <neniusgediminas@gmail.com>
2021-06-01 13:40:29 +0300

    optimized code by adding reccurring values object

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -1,34 +1,40 @@
 import path from 'path';
 
+const recValues = {
+  type: 'mysql',
+  entities: [path.join(__dirname, 'src/Entities/*.ts')],
+  synchronize: true,
+};
+
 export default {
   production: {
-    type: 'mysql',
+    type: recValues.type,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: [path.join(__dirname, 'src/Entities/*.ts')],
-    synchronize: true,
+    entities: recValues.entities,
+    synchronize: recValues.synchronize,
   },
   development: {
-    type: 'mysql',
+    type: recValues.type,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: [path.join(__dirname, 'src/Entities/*.ts')],
-    synchronize: true,
+    entities: recValues.entities,
+    synchronize: recValues.synchronize,
   },
   testing: {
-    type: 'mysql',
+    type: recValues.type,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: [path.join(__dirname, 'src/Entities/*.ts')],
-    synchronize: true,
+    entities: recValues.entities,
+    synchronize: recValues.synchronize,
   },
 };
