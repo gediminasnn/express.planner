b0b4e74a8333b8b14d54ae29d5b8ec8f02357302
gediminasnn <neniusgediminas@gmail.com>
2021-06-01 13:46:31 +0300

    added spread operator to env objects

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-const recValues = {
+const recurringValues = {
   type: 'mysql',
   entities: [path.join(__dirname, 'src/Entities/*.ts')],
   synchronize: true,
@@ -8,33 +8,27 @@ const recValues = {
 
 export default {
   production: {
-    type: recValues.type,
+    ...recurringValues,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: recValues.entities,
-    synchronize: recValues.synchronize,
   },
   development: {
-    type: recValues.type,
+    ...recurringValues,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: recValues.entities,
-    synchronize: recValues.synchronize,
   },
   testing: {
-    type: recValues.type,
+    ...recurringValues,
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    entities: recValues.entities,
-    synchronize: recValues.synchronize,
   },
 };
