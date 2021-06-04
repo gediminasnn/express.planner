645314add9b20b99b8f640b453ecc3b35db79a47
gediminasnn <neniusgediminas@gmail.com>
2021-06-04 16:28:41 +0300

    added data aggregation

--- src/Controllers/User.ts
+++ src/Controllers/User.ts
@@ -39,9 +39,33 @@ export default class UserController implements Controller {
     }
   }
 
-  public async getUsers(_, res: Response) {
+  public async getUsers(req: Request, res: Response) {
+    let { order, start, limit } = { ...req.query } as unknown as {
+      order: string | undefined;
+      start: number | undefined;
+      limit: number | undefined;
+    };
+
+    if (order === undefined) {
+      order = 'DESC';
+    }
+    if (start === undefined) {
+      start = 0;
+    }
+    if (limit === undefined) {
+      limit = 10;
+    }
+
     try {
-      const users = await this.userRepository.find();
+      let users = await this.userRepository.find();
+
+      if (order === 'ASC') {
+        users = users.sort((a, b) => (a.createdAt.getTime() > b.createdAt.getTime() ? 1 : -1));
+      } else if (order === 'DESC') {
+        users = users.sort((a, b) => (b.createdAt.getTime() > a.createdAt.getTime() ? 1 : -1));
+      }
+
+      users = users.slice(start - 1, limit);
 
       return res.status(200).json(users);
     } catch (e) {
