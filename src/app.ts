6076f37c45636eac9c1c4b74ff0d0623c5eacf1e
gediminasnn <neniusgediminas@gmail.com>
2021-06-11 10:32:25 +0300

    added authcontroller to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { initializeConnection } from './Utils/app';
 import Bootstrap from './Bootstrap';
 import UserController from './Modules/User/User.controller';
+import AuthController from './Modules/Auth/Auth.controller';
 
 (async () => {
   try {
     await initializeConnection();
 
-    const app = new Bootstrap([new UserController()]);
+    const app = new Bootstrap([new UserController(), new AuthController()]);
 
     app.listen();
   } catch (e) {
